3ab4508122f32ac65b4a69d516464d5d1fbc0867
erokn <erokin@mail.ru>
2022-07-13 22:34:23 +0400

    Task_final

--- task_final/solution.js
+++ task_final/solution.js
@@ -13,8 +13,8 @@
 // - order - объект, содержащий данные о заказе:
 //     - address - строка с адресом доставки, записанным человекопонятным языком (как в примере)
 //     - sum - стоимость заказа с учетом скидок и доставки
-// - goods: массив объектов с информацией о позициях заказа:
-//     - title - название позиции
+// - goods: массив объектов с информацией о позициях заказа: 
+//     - title - название позиции/
 //     - count - количество в заказе
 // например:
 // {
@@ -33,20 +33,24 @@
 //    }
 // }
 
+
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    let data = {
+        data:{
+        "client": `${name} ${phone}`,
+        "order": {
+                address: `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`,
+                sum: sum
+        },
+        "goods": []
+        }
+    };
 
-    let countOfGoods = goods.length;
-
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let i = 0; i < goods.length; i += 1) {
+        data.data.goods.push({"title": goods[i].title, "count":goods[i].count});
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
+ 
     let jsonData = JSON.stringify(data);
 
     return jsonData;
